03b6c41048dff2304f7ae3b25efa52f29609f79e
traviscult <60018617+traviscult@users.noreply.github.com>
2023-04-04 16:00:18 -0500

    Updated header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,10 @@ const Header = () => {
   return (
     <div className="header">
       <h1>Employee Directory</h1>
+      <p>
+        Click on name filed to filter alphabetically or use the search box to
+        narrow your results.
+      </p>
     </div>
   );
 };
