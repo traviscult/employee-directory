d08a13a26517eecd16eb78d26abd7ad9d1df1796
traviscult <60018617+traviscult@users.noreply.github.com>
2023-04-04 16:18:44 -0500

    added css styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import "./Header.css";
 const Header = () => {
   return (
     <div className="header">
-      <h1>Employee Directory</h1>
+      <h1 className="title">Employee Directory</h1>
       <p>
         Click on name filed to filter alphabetically or use the search box to
         narrow your results.
