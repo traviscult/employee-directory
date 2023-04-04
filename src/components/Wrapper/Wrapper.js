3606d82b3c07879c12484d624396a0ca9eb6210e
traviscult <60018617+traviscult@users.noreply.github.com>
2023-04-04 15:47:55 -0500

    added api call to wrapper

--- src/components/Wrapper/Wrapper.js
+++ src/components/Wrapper/Wrapper.js
@@ -12,6 +12,67 @@ class Wrapper extends Component {
     filteredEmployees: [],
     order: "",
   };
+  // this is what will be displayed when page is loaded
+  componentDidMount() {
+    API.getUsers()
+      .then((res) =>
+        this.setState({
+          employees: res.data.results,
+          filteredEmployees: res.data.results,
+        })
+      )
+      .catch((err) => console.log(err));
+  }
+
+  sortByName = () => {
+    const filtereds = this.state.filteredEmployees;
+    if (this.state.order === "asc") {
+      const sorteds = filtereds.sort((a, b) =>
+        a.name.first > b.name.first ? 1 : -1
+      );
+      console.log(sorteds);
+
+      this.setState({
+        filteredEmployees: sorteds,
+        order: "desc",
+      });
+    } else {
+      const sorteds = filtereds.sort((a, b) =>
+        a.name.first > b.name.first ? -1 : 1
+      );
+      console.log(sorteds);
+
+      this.setState({
+        filteredEmployees: sorteds,
+        order: "asc",
+      });
+    }
+  };
+
+  handleInputChange = (event) => {
+    const employees = this.state.employees;
+    const UserInput = event.target.value;
+    const filteredEmployees = employees.filter(
+      (employee) =>
+        employee.name.first.toLowerCase().indexOf(UserInput.toLowerCase()) > -1
+    );
+    this.setState({
+      filteredEmployees,
+    });
+  };
+
+  // API call to randomuser.me
+  employeeSearch = () => {
+    API.getUsers()
+      .then((res) => {
+        console.log("employee data", res);
+        this.setState({
+          employees: res.data.results,
+          filteredEmployees: res.data.results,
+        });
+      })
+      .catch((err) => console.log(err));
+  };
 }
 
 export default Wrapper;
