d2fc241c26c079f985381459a8de93def5bf7772
traviscult <60018617+traviscult@users.noreply.github.com>
2023-04-04 16:22:06 -0500

    updated api path

--- src/components/Wrapper/Wrapper.js
+++ src/components/Wrapper/Wrapper.js
@@ -1,4 +1,4 @@
-import API from "../../utils/API";
+import API from "../../utils/API.js";
 import Header from "../Header/Header";
 import EmployeeTable from "../EmployeeTable/EmployeeTable";
 import SearchBar from "../SearchBar/SearchBar";
